344d8f0f63201196824a15a8efb29cf5c46cb01e
Sergio Garrido <sergarridom8@gmail.com>
2023-10-19 17:19:52 +0200

    Fix on createRoom()

--- lib/controllers/hls_controller.dart
+++ lib/controllers/hls_controller.dart
@@ -6,10 +6,11 @@ class HLSController {
   final HLSService _hlsService = HLSService();
 
   HLSController(this.excursionController);
-  createRoom() async {
+  Future<String> createRoom() async {
     try {
       final String roomId = await _hlsService.createRoom();
       await excursionController.addStreamingRoom(roomId);
+      return roomId;
     } catch (e) {
       throw Exception(
           'Hubo un error al crear la retransmisión: ${e.toString()}');
