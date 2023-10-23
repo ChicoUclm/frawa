5f0f1cf5bb8e033673c12e26b2b0a054b82f43ed
Sergio Garrido <sergarridom8@gmail.com>
2023-10-23 13:10:06 +0200

    Methods for handling active room

--- lib/controllers/hls_controller.dart
+++ lib/controllers/hls_controller.dart
@@ -1,19 +1,32 @@
 import 'package:excursiona/controllers/excursion_controller.dart';
+import 'package:excursiona/helper/helper_functions.dart';
 import 'package:excursiona/services/hls_service.dart';
 
 class HLSController {
-  final ExcursionController excursionController;
+  final ExcursionController? excursionController;
   final HLSService _hlsService = HLSService();
 
-  HLSController(this.excursionController);
+  HLSController({this.excursionController});
   Future<String> createRoom() async {
     try {
       final String roomId = await _hlsService.createRoom();
-      await excursionController.addStreamingRoom(roomId);
+      await excursionController!.addStreamingRoom(roomId);
       return roomId;
     } catch (e) {
       throw Exception(
           'Hubo un error al crear la retransmisión: ${e.toString()}');
     }
+  }
+
+  setActiveStreamingRoom(String roomId) async {
+    await HelperFunctions.setActiveStreamingRoom(roomId);
+  }
+
+  getActiveStreamingRoom() async {
+    return await HelperFunctions.getActiveStreamingRoom();
+  }
+
+  deleteActiveStreamingRoom() async {
+    return await HelperFunctions.deleteActiveStreamingRoom();
   }
 }
