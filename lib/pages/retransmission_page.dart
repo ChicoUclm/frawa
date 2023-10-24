835a4717ef817c8f415efd781ae61381793c7025
Sergio Garrido <sergarridom8@gmail.com>
2023-10-24 14:03:04 +0200

    Retrieve and list active streamings

--- lib/pages/retransmission_page.dart
+++ lib/pages/retransmission_page.dart
@@ -1,8 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:excursiona/controllers/excursion_controller.dart';
 import 'package:excursiona/controllers/hls_controller.dart';
+import 'package:excursiona/controllers/user_controller.dart';
+import 'package:excursiona/model/livestreaming_room.dart';
+import 'package:excursiona/model/user_model.dart';
 import 'package:excursiona/pages/videostreaming_page.dart';
 import 'package:excursiona/shared/constants.dart';
 import 'package:excursiona/shared/utils.dart';
+import 'package:excursiona/widgets/account_avatar.dart';
+import 'package:excursiona/widgets/loader.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
@@ -31,7 +37,7 @@ class _RetransmissionsPageState extends State<RetransmissionsPage> {
       final roomId =
           await HLSController(excursionController: widget.excursionController)
               .createRoom();
-      nextScreen(
+      nextScreenReplace(
           context,
           VideoStreamingPage(
               roomId: roomId,
@@ -79,13 +85,14 @@ class _RetransmissionsPageState extends State<RetransmissionsPage> {
                       style: TextStyle(color: Colors.white)),
                   IconButton(
                     onPressed: () {
-                      nextScreen(
+                      nextScreenReplace(
                           context,
                           VideoStreamingPage(
-                              roomId: roomId!,
-                              token: Constants.tempStreamingToken,
-                              mode: Mode.CONFERENCE,
-                              excursionController: widget.excursionController),
+                            roomId: roomId!,
+                            token: Constants.tempStreamingToken,
+                            mode: Mode.CONFERENCE,
+                            excursionController: widget.excursionController,
+                          ),
                           PageTransitionType.fade);
                     },
                     icon: const Icon(Icons.arrow_forward_ios_rounded),
@@ -95,7 +102,28 @@ class _RetransmissionsPageState extends State<RetransmissionsPage> {
                 ],
               ),
             ),
-          )
+          ),
+        Expanded(
+          child: FutureBuilder(
+              future: widget.excursionController.getStreamingRooms(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return snapshot.data!.isEmpty
+                      ? const Center(
+                          child: Text(
+                              'No hay ninguna retransmisión en directo ahora mismo'),
+                        )
+                      : ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            var room = snapshot.data![index];
+                            return StreamingRoomTile(room);
+                          });
+                } else {
+                  return const Loader();
+                }
+              }),
+        ),
       ]),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Constants.indigoDye,
@@ -103,5 +131,76 @@ class _RetransmissionsPageState extends State<RetransmissionsPage> {
         child: const Icon(Icons.video_call_rounded),
       ),
     );
+  }
+}
+
+class StreamingRoomTile extends StatefulWidget {
+  final LiveStreamingRoom room;
+  const StreamingRoomTile(this.room, {super.key});
+
+  @override
+  State<StreamingRoomTile> createState() => _StreamingRoomTileState();
+}
+
+class _StreamingRoomTileState extends State<StreamingRoomTile> {
+  late UserModel userInfo;
+  bool _hasLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    UserController().getUserDataById(widget.room.userId).then((userModel) {
+      setState(() {
+        userInfo = userModel;
+        _hasLoaded = true;
+      });
+    }).catchError((e) {
+      showSnackBar(context, Colors.red, e.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !_hasLoaded
+        ? Container()
+        : ListTile(
+            leading: userInfo.profilePic == ''
+                ? CircleAvatar(
+                    foregroundColor: Colors.grey,
+                    backgroundColor: Colors.transparent,
+                    radius: 30,
+                    child: AccountAvatar(radius: 30, name: userInfo.name))
+                : CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.transparent,
+                    child: CachedNetworkImage(
+                      imageUrl: userInfo.profilePic,
+                      placeholder: (context, url) => const Loader(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                      placeholderFadeInDuration:
+                          const Duration(milliseconds: 300),
+                      imageBuilder: (context, imageProvider) => Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: imageProvider, fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                  ),
+            trailing: IconButton(
+              icon: const Icon(Icons.play_arrow_rounded),
+              onPressed: () => nextScreenReplace(
+                  context,
+                  VideoStreamingPage(
+                    roomId: widget.room.roomId,
+                    token: Constants.tempStreamingToken,
+                    mode: Mode.VIEWER,
+                  ),
+                  PageTransitionType.rightToLeft),
+            ),
+            title: Text(userInfo.name),
+          );
   }
 }
