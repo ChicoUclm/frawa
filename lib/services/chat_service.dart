3d307f0d4793b6f625a2f6fe2a95434e87c4d236
Sergio Garrido <sergarridom8@gmail.com>
2023-11-28 13:41:26 +0100

    Removed unused models and function

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,38 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:excursiona/model/chat_contact.dart';
 import 'package:excursiona/model/message.dart';
-import 'package:excursiona/model/user_model.dart';
-import 'package:excursiona/services/user_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
-  CollectionReference _excursionCollection =
+  final CollectionReference _excursionCollection =
       FirebaseFirestore.instance.collection('excursions');
-
-  Stream<List<ChatContact>> getChatsContacts() {
-    return UserService()
-        .userCollection
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .collection('chats')
-        .snapshots()
-        .asyncMap((snapshot) async {
-      List<ChatContact> chatContacts = [];
-      for (var doc in snapshot.docs) {
-        var chatContact = ChatContact.fromMap(doc.data());
-        var userData =
-            await UserService().userCollection.doc(chatContact.contactID).get();
-        var user = UserModel.fromMap(userData.data()! as Map<String, dynamic>);
-
-        chatContacts.add(ChatContact(
-            name: user.name,
-            profilePic: user.profilePic,
-            contactID: user.uid,
-            timeSent: chatContact.timeSent,
-            lastMessage: chatContact.lastMessage));
-      }
-      return chatContacts;
-    });
-  }
 
   sendGroupMessage(
       {required String excursionId, required Message message}) async {
@@ -55,12 +26,10 @@ class ChatService {
           .orderBy('timeSent')
           .snapshots()
           .map((snapshot) {
-        return snapshot.docs
-            .map((doc) => Message.fromMap(doc.data() as Map<String, dynamic>))
-            .toList();
+        return snapshot.docs.map((doc) => Message.fromMap(doc.data())).toList();
       });
     } catch (e) {
-      return Stream.empty();
+      return const Stream.empty();
     }
   }
 }
