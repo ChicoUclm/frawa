31bd19131e53ed8802b86da807b499c7557984cb
Manuel Ortega <manuel.ortega.cordovilla@gmail.com>
2024-04-18 08:01:38 +0200

    Add support for Google's tools on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
 import UIKit
 import Flutter
 
+import GoogleMaps
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBLqUILCh7PBVWO-Ky4N6-STg4a-0xsjV8")
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
